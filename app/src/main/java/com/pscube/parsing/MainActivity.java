77e0a518b31cf57f5204cb67e886f97215f61d71
possible3656 <sj94611@gmail.com>
2020-03-27 19:29:13 +0530

    first we use jsonObjectRequest now wwe have used jsonArrayRequest
    procedure is same just use jsonArrayRequest instead of jsonObjectRequest

--- app/src/main/java/com/pscube/parsing/MainActivity.java
+++ app/src/main/java/com/pscube/parsing/MainActivity.java
@@ -9,9 +9,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 //coding team = sambhav jain
@@ -25,25 +27,50 @@ public class MainActivity extends AppCompatActivity {
 
         requestQueue=Volley.newRequestQueue(this);
 
+//
+//        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
+//                "https://jsonplaceholder.typicode.com/users/1", null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                try {
+//                    Log.d("happy","RESULT "+ response.getString("name"));
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.d("sad","RESULT"+error.getMessage());
+//            }
+//        });
 
-        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
-                "https://jsonplaceholder.typicode.com/users/1", null, new Response.Listener<JSONObject>() {
+
+        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/users", null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    Log.d("happy","RESULT "+ response.getString("name"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public void onResponse(JSONArray response) {
+                Log.d("happy",""+response);
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("sad","RESULT"+error.getMessage());
+                Log.d("sad",""+error.getMessage());
+
             }
         });
-        requestQueue.add(objectRequest);
+
+
+
+
+
+
+
+    requestQueue.add(jsonArrayRequest);
+
+       // requestQueue.add(objectRequest);
 
 
 
