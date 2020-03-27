b121848e0b49c9bc7413331524337d3207abee29
possible3656 <sj94611@gmail.com>
2020-03-27 18:50:36 +0530

    Initial commit

---
+++ app/src/main/java/com/pscube/parsing/MainActivity.java
@@ -0,0 +1,53 @@
+package com.pscube.parsing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestQueue=Volley.newRequestQueue(this);
+
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/users/1", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("happy","RESULT"+response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("sad","RESULT"+error.getMessage());
+            }
+        });
+        requestQueue.add(objectRequest);
+
+
+
+
+
+
+
+
+
+    }
+
+}
